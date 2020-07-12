3523d1328341dcf872f059f87096cc333a6d2b98
dini_razak <drazak97@gmail.com>
2020-07-13 02:29:27 +0800

    * update travel.sql

--- travel.sql
+++ travel.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 12, 2020 at 04:04 PM
+-- Generation Time: Jul 12, 2020 at 08:28 PM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.2.27
 
@@ -25,62 +25,53 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `admin`
---
-
-CREATE TABLE `admin` (
-  `id` int(11) NOT NULL,
-  `username` varchar(25) NOT NULL,
-  `password` varchar(25) NOT NULL,
-  `name` varchar(150) NOT NULL,
-  `email` varchar(250) NOT NULL,
-  `phone_number` int(15) NOT NULL,
-  `addeddate` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `booking`
 --
 
 CREATE TABLE `booking` (
   `id` int(11) NOT NULL,
-  `name` varchar(50) NOT NULL,
-  `depature` date NOT NULL,
-  `arrival` date NOT NULL,
-  `class` int(1) NOT NULL,
-  `adult` int(2) NOT NULL,
-  `children` int(2) NOT NULL,
-  `destination` int(2) NOT NULL,
+  `foreign_key` int(11) NOT NULL,
   `status` int(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `member`
+-- Table structure for table `destination`
 --
 
-CREATE TABLE `member` (
+CREATE TABLE `destination` (
+  `id` int(11) NOT NULL,
+  `depature` date NOT NULL,
+  `arrival` date NOT NULL,
+  `class` int(1) NOT NULL,
+  `adult` int(2) NOT NULL,
+  `children` int(2) NOT NULL,
+  `status` int(1) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `destination` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
   `id` int(11) NOT NULL,
   `username` varchar(25) NOT NULL,
   `password` varchar(25) NOT NULL,
   `phone_number` int(12) NOT NULL,
   `email` varchar(50) NOT NULL,
   `full_name` varchar(50) NOT NULL,
-  `addeddate` datetime NOT NULL
+  `addeddate` datetime NOT NULL,
+  `role` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `admin`
---
-ALTER TABLE `admin`
-  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `booking`
@@ -89,20 +80,20 @@ ALTER TABLE `booking`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `member`
+-- Indexes for table `destination`
 --
-ALTER TABLE `member`
+ALTER TABLE `destination`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
   ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
-
---
--- AUTO_INCREMENT for table `admin`
---
-ALTER TABLE `admin`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `booking`
@@ -111,9 +102,15 @@ ALTER TABLE `booking`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `member`
+-- AUTO_INCREMENT for table `destination`
 --
-ALTER TABLE `member`
+ALTER TABLE `destination`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
